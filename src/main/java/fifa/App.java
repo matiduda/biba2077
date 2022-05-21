f8eedfcf1cf7ed1a82924ee3fa438a6b9477cce3
Boomber863 <boomber8634@gmail.com>
2022-05-21 17:10:08 +0200

    Boisko i bramki

--- src/main/java/fifa/App.java
+++ src/main/java/fifa/App.java
@@ -2,11 +2,17 @@ package fifa;
 
 import javafx.application.Application;
 import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Polygon;
+import javafx.scene.shape.Polyline;
 import javafx.stage.Stage;
+
+import static java.lang.Math.sqrt;
 
 
 /**
@@ -18,8 +24,8 @@ public class App extends Application {
     boolean STARTS_FULLSCREEN = false;
     boolean IS_RESIZABLE = false;
 
-    int width = 800;
-    int height = 800;
+    final static double width = 1200;
+    final static double height = 800;
     //
 
     public static void main(String[] args) {
@@ -28,21 +34,98 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        // Automatically called by Application.launch()
-        
-        // Add a Scene
+
         Group root = new Group();
 
         Scene scene = new Scene(root, width, height, Color.TRANSPARENT);
 
+        Circle ground = new Circle(width/2,height/2,height/2, Color.SADDLEBROWN);
+
+        Polygon field = new Polygon();
+        field.getPoints().addAll(width/2 - height/2,height/2,
+               width/2 - height/4, height/2 + (height/2 * sqrt(3))/2,
+               width/2 + height/4, height/2 + (height/2 * sqrt(3))/2,
+               width/2 + height/2, height/2,
+               width/2 + height/4, height/2 - (height/2 * sqrt(3)/2),
+               width/2 - height/4, height/2 - (height/2 * sqrt(3))/2);
+        field.setFill(Color.LAWNGREEN);
+        field.setStroke(Color.WHITE);
+        field.setStrokeWidth(5);
+
+        Polyline goal1 = createGoal(1);
+        Polyline goal2 = createGoal(2);
+        Polyline goal3 = createGoal(3);
+
+        /*Polyline bramka1 = new Polyline();
+        Polyline bramka2 = new Polyline();
+        Polyline bramka3 = new Polyline();
+
+        bramka1.getPoints().addAll(midpointX(width/2 - height/2,midpointX(width/2 - height/2, width/2 - height/4))+height/20,  midpointY(height/2, midpointY(height/2,height/2 - (height/2 * sqrt(3))/2))+height/40,
+                midpointX(width/2 - height/2,midpointX(width/2 - height/2, width/2 - height/4)),  midpointY(height/2, midpointY(height/2,height/2 - (height/2 * sqrt(3))/2)),
+                midpointX(width/2 - height/4,midpointX(width/2 - height/2, width/2 - height/4)),  midpointY(height/2 - (height/2 * sqrt(3))/2, midpointY(height/2,height/2 - (height/2 * sqrt(3))/2)),
+                midpointX(width/2 - height/4,midpointX(width/2 - height/2, width/2 - height/4))+height/20,  midpointY(height/2 - (height/2 * sqrt(3))/2, midpointY(height/2,height/2 - (height/2 * sqrt(3))/2))+height/40);
+
+        bramka2.getPoints().addAll(midpointX(width/2 + height/2,midpointX(width/2 + height/2, width/2 + height/4))-height/20,  midpointY(height/2, midpointY(height/2,height/2 - (height/2 * sqrt(3))/2))+height/40,
+                midpointX(width/2 + height/2,midpointX(width/2 + height/2, width/2 + height/4)),  midpointY(height/2, midpointY(height/2,height/2 - (height/2 * sqrt(3))/2)),
+                midpointX(width/2 + height/4,midpointX(width/2 + height/2, width/2 + height/4)),  midpointY(height/2 - (height/2 * sqrt(3))/2, midpointY(height/2,height/2 - (height/2 * sqrt(3))/2)),
+                midpointX(width/2 + height/4,midpointX(width/2 + height/2, width/2 + height/4))-height/20,  midpointY(height/2 - (height/2 * sqrt(3))/2, midpointY(height/2,height/2 - (height/2 * sqrt(3))/2))+height/40);
+
+        bramka3.getPoints().addAll(midpointX(width/2 - height/2,midpointX(width/2 - height/2, width/2 - height/4))+height/20,  midpointY(height/2, midpointY(height/2,height/2 - (height/2 * sqrt(3))/2))+height/40,
+                midpointX(width/2 - height/2,midpointX(width/2 - height/2, width/2 - height/4)),  midpointY(height/2, midpointY(height/2,height/2 - (height/2 * sqrt(3))/2)),
+                midpointX(width/2 - height/4,midpointX(width/2 - height/2, width/2 - height/4)),  midpointY(height/2 - (height/2 * sqrt(3))/2, midpointY(height/2,height/2 - (height/2 * sqrt(3))/2)),
+                midpointX(width/2 - height/4,midpointX(width/2 - height/2, width/2 - height/4))+height/20,  midpointY(height/2 - (height/2 * sqrt(3))/2, midpointY(height/2,height/2 - (height/2 * sqrt(3))/2))+height/40);
+
+        bramka1.setFill(Color.BLUE);
+        bramka1.setStrokeWidth(5);
+        bramka1.setFill(Color.BLUE);
+        bramka1.setStrokeWidth(5);*/
+
         Player player = new Player(stage, scene, 50, 50);
-        Ball ball = new Ball(400,400);
+        Ball ball = new Ball((int) width/2,(int) height/2);
         new CollisionDetection(ball, player);
-        root.getChildren().add(player.ball);
-        root.getChildren().add(ball.ball);
+        root.getChildren().addAll(ground, field, goal1, goal2, goal3, player.ball, ball.ball);
 
         prepareGameWindow(stage, scene, "/icon/icon.png");
     }
+
+    private Polyline createGoal(int type){
+        Polyline goal = new Polyline();
+        switch (type) {
+            case 1:
+                goal.getPoints().addAll(midpointX(width/2 - height/2,midpointX(width/2 - height/2, width/2 - height/4))+height/20,  midpointY(height/2, midpointY(height/2,height/2 - (height/2 * sqrt(3))/2))+height/40,
+                        midpointX(width/2 - height/2,midpointX(width/2 - height/2, width/2 - height/4)),  midpointY(height/2, midpointY(height/2,height/2 - (height/2 * sqrt(3))/2)),
+                        midpointX(width/2 - height/4,midpointX(width/2 - height/2, width/2 - height/4)),  midpointY(height/2 - (height/2 * sqrt(3))/2, midpointY(height/2,height/2 - (height/2 * sqrt(3))/2)),
+                        midpointX(width/2 - height/4,midpointX(width/2 - height/2, width/2 - height/4))+height/20,  midpointY(height/2 - (height/2 * sqrt(3))/2, midpointY(height/2,height/2 - (height/2 * sqrt(3))/2))+height/40);
+                goal.setFill(Color.BLUE);
+                goal.setStrokeWidth(5);
+                break;
+            case 2:
+                goal.getPoints().addAll(midpointX(width/2 + height/2,midpointX(width/2 + height/2, width/2 + height/4))-height/20,  midpointY(height/2, midpointY(height/2,height/2 - (height/2 * sqrt(3))/2))+height/40,
+                        midpointX(width/2 + height/2,midpointX(width/2 + height/2, width/2 + height/4)),  midpointY(height/2, midpointY(height/2,height/2 - (height/2 * sqrt(3))/2)),
+                        midpointX(width/2 + height/4,midpointX(width/2 + height/2, width/2 + height/4)),  midpointY(height/2 - (height/2 * sqrt(3))/2, midpointY(height/2,height/2 - (height/2 * sqrt(3))/2)),
+                        midpointX(width/2 + height/4,midpointX(width/2 + height/2, width/2 + height/4))-height/20,  midpointY(height/2 - (height/2 * sqrt(3))/2, midpointY(height/2,height/2 - (height/2 * sqrt(3))/2))+height/40);
+                goal.setFill(Color.YELLOW);
+                goal.setStrokeWidth(5);
+                break;
+            case 3:
+                goal.getPoints().addAll(midpointX(width/2 + height/4,midpointX(width/2 + height/4, width/2 - height/4)),  height/2 + (height/2 * sqrt(3)/2) - height/20,
+                        midpointX(width/2 + height/4,midpointX(width/2 + height/4, width/2 - height/4)),  height/2 + (height/2 * sqrt(3)/2),
+                        midpointX(width/2 - height/4,midpointX(width/2 + height/4, width/2 - height/4)),  height/2 + (height/2 * sqrt(3)/2),
+                        midpointX(width/2 - height/4,midpointX(width/2 + height/4, width/2 - height/4)),  height/2 + (height/2 * sqrt(3)/2) -height/20);
+                goal.setFill(Color.RED);
+                goal.setStrokeWidth(5);
+                break;
+        }
+        return goal;
+    }
+
+    private double midpointX(double x1, double x2) {
+       return (x1+x2)/2;
+    }
+    private double midpointY(double y1, double y2) {
+        return (y1+y2)/2;
+    }
+
 
 
     private void prepareGameWindow(Stage stage, Scene scene, String pathToIcon) {
