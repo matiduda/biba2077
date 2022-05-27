0d15b6fd12273563ae49185c4c898a12a946d79a
Mateusz Duda <mateusz.duda2@gmail.com>
2022-05-27 14:04:46 +0200

    added game status bar

--- src/main/java/fifa/Logic.java
+++ src/main/java/fifa/Logic.java
@@ -1,14 +1,17 @@
 package fifa;
 
+import java.io.IOException;
+
 import javafx.animation.AnimationTimer;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.CycleMethod;
-import javafx.scene.paint.Paint;
 import javafx.scene.paint.RadialGradient;
 import javafx.scene.paint.Stop;
 import javafx.scene.text.Font;
@@ -16,29 +19,31 @@ import javafx.scene.text.Font;
 public class Logic {
 
     private Label gameTime;
-    private int timeInSeconds = 0;
-    private double roundTime;
+    private long timeInSeconds = 0;
 
     private final double timerX = 20;
     private final double timerY = 30;
-    
+
+    private GridPane panel;
+
     public Logic(double roundTime, Elements list) {
         gameTime = new Label("00:00");
         gameTime.setLayoutX(timerX);
         gameTime.setLayoutY(timerY);
         gameTime.setFont(Font.font("Verdana", 30));
 
-    BackgroundFill fill = new BackgroundFill(
-            new RadialGradient(
-                    0, 0, 0.5, 0.5, 0.5, true,
-                    CycleMethod.NO_CYCLE,
-                    new Stop(0, Color.web("#FA432233")),
-                    new Stop(1, Color.web("#00000033"))),
-            CornerRadii.EMPTY, Insets.EMPTY
-    );
+        BackgroundFill fill = new BackgroundFill(
+                new RadialGradient(
+                        0, 0, 0.5, 0.5, 0.5, true,
+                        CycleMethod.NO_CYCLE,
+                        new Stop(0, Color.web("#FA432233")),
+                        new Stop(1, Color.web("#00000033"))),
+                CornerRadii.EMPTY, Insets.EMPTY);
         gameTime.setBackground(new Background(fill));
         list.add(gameTime);
+        // list.add(panel);
 
+        // loadGameBar();
         setEventsAndTimers();
     }
 
@@ -49,13 +54,18 @@ public class Logic {
             @Override
             public void handle(long now) {
                 timeInSeconds++;
-
-                int milis = timeInSeconds % 60;
-                int seconds = timeInSeconds / 60;
-                int minutes = timeInSeconds / 3600;
-                gameTime.setText(String.format("%02d:%02d:%02d", minutes, seconds, milis));
+                gameTime.setText(String.format("%02d:%02d", timeInSeconds / 3600, timeInSeconds / 60));
             }
         };
         timer.start();
     }
+
+    // private void loadGameBar() {
+    //     FXMLLoader loader = new FXMLLoader(getClass().getResource("/gamebar/bar.fxml"));
+    //     try {
+    //         panel = (GridPane) loader.load();
+    //     } catch (IOException e) {
+    //         e.printStackTrace();
+    //     }
+    // }
 }
